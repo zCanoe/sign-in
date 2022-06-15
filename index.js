e67879ed6046b91c567038d9cee7e60c3b550371
chinazhoujunbo <43891961+chinazhoujunbo@users.noreply.github.com>
2022-06-15 13:34:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const data = {
     'jzdXian.dm':	'430104',
     'jzdDz':	`学校`,
     'jzdDz2':	`湖南省长沙市`,
-    'lxdh':	`18569545194`,
+    'lxdh':	`1856954519`,
     'sfzx':	'1',
     'sfzx1':	`在校`,
     'twM.dm': "01",
